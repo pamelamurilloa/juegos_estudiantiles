bdc2092c4b0c889a5c2f917265634ac5c3d33560
Pamela Murillo <pamelamurilloa@gmail.com>
2022-07-18 10:46:29 -0600

    se ha iniciado los archivos para ahorcado

--- juegos_utn_corralesmurillo/src/main/java/hanged_game/GameLogicHanged.java
+++ juegos_utn_corralesmurillo/src/main/java/hanged_game/GameLogicHanged.java
@@ -1,13 +1,15 @@
 package hanged_game;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class GameLogicHanged {
     Scanner read = new Scanner(System.in);
     
-    public static String getWordFromFile(int amountChar) { //this method gets a random word from the file, with X number of characters
-        System.out.println("");
-        return amountChar + "";
+    public static String getWordFromFile(int amountChar) throws IOException { //this method gets a random word from the file, with X number of characters
+        ArchiveReader archiveReader = new ArchiveReader();
+        archiveReader.createFile();
+        return archiveReader.readInFile(amountChar);
     }
     
     
@@ -24,37 +26,42 @@ public class GameLogicHanged {
         
         letter = letter.toUpperCase();
         userWord = userWord.toUpperCase(); //I uppercased everything to maintaing homogeneity
-        String[] array2 = shownWord.split(""); //make an array to show eventually to the user
+        String[] wordArray = shownWord.split(""); //make an array to show eventually to the user
         
         int position = userWord.indexOf(letter);
         
         if (userWord.contains(letter)) {
             for(int i = 0; userWord.length() > i; i++) {
                 if (i == position) {  // The index is the same position as one of the char
-                    array2[i] = letter;
+                    wordArray[i] = letter;
                     position = userWord.indexOf(letter, position + 1);
                 }
            }
         }
-        return convertToString(array2);
+        return convertToString(wordArray);
     }
 
     
-//    public static void main(String[] args) {
-//        String shownWord = "***********";
-//        shownWord = checkChar("A", "Mississippi", shownWord);
-//        System.out.println(shownWord);
-//        
-//        shownWord = checkChar("P", "Mississippi", shownWord);
-//        System.out.println(shownWord);
-//        
-//        shownWord = checkChar("o", "Mississippi", shownWord);
-//        System.out.println(shownWord);
-//        
-//        shownWord = checkChar("M", "Mississippi", shownWord);
-//        System.out.println(shownWord);
-//        
-//        shownWord = checkChar("s", "Mississippi", shownWord);
-//        System.out.println(shownWord);
-//    }
+    public static void main(String[] args) throws IOException {
+        String shownWord = "***********";
+        shownWord = checkChar("A", "Mississippi", shownWord);
+        System.out.println(shownWord);
+        
+        shownWord = checkChar("P", "Mississippi", shownWord);
+        System.out.println(shownWord);
+        
+        shownWord = checkChar("o", "Mississippi", shownWord);
+        System.out.println(shownWord);
+        
+        shownWord = checkChar("M", "Mississippi", shownWord);
+        System.out.println(shownWord);
+        
+        shownWord = checkChar("s", "Mississippi", shownWord);
+        System.out.println(shownWord);
+        
+        String a = getWordFromFile(5);
+        System.out.println(a);
+    }
+    
+    
 }
