ef1c8e20984de9cb08804f23cf54e03e65682b03
Pamela Murillo <pamelamurilloa@gmail.com>
2022-07-18 09:27:56 -0600

    El juego funciona en consola

--- juegos_utn_corralesmurillo/src/main/java/hanged_game/GameLogicHanged.java
+++ juegos_utn_corralesmurillo/src/main/java/hanged_game/GameLogicHanged.java
@@ -1,16 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hanged_game;
 
-/**
- *
- * @author pamelamurillo
- */
-public class GameLogicHanged { 
-    public static String newRound() {
+import java.util.Scanner;
+
+public class GameLogicHanged {
+    Scanner read = new Scanner(System.in);
+    
+    public static String getWordFromFile(int amountChar) { //this method gets a random word from the file, with X number of characters
         System.out.println("");
-        return "";
+        return amountChar + "";
+    }
+    
+    public static String convertToString(String[] array){
+        String temporalWord = "";
+        for (int i = 0; array.length > i; i ++) {
+            temporalWord += array[i];
+        }
+        return temporalWord;
+    }
+    
+    public static String checkChar(String letter, String userWord, String shownWord) { //this method verifies a char input from the user is in the word, and returns the string without those blank spaces
+        
+        letter = letter.toUpperCase();
+        userWord = userWord.toUpperCase();
+        String[] array2 = shownWord.split("");
+        
+        int position = userWord.indexOf(letter);
+        
+        if (userWord.contains(letter)) {
+            for(int i = 0; userWord.length() > i; i++) {
+                if (i == position) {  // The index is the same position as one of the char
+                    array2[i] = letter;
+                    position = userWord.indexOf(letter, position + 1);
+                }
+           }
+        }
+        return convertToString(array2);
+    }
+
+    
+    public static void main(String[] args) {
+        String shownWord = "***********";
+        shownWord = checkChar("A", "Mississippi", shownWord);
+        System.out.println(shownWord);
+        
+        shownWord = checkChar("P", "Mississippi", shownWord);
+        System.out.println(shownWord);
+        
+        shownWord = checkChar("o", "Mississippi", shownWord);
+        System.out.println(shownWord);
+        
+        shownWord = checkChar("M", "Mississippi", shownWord);
+        System.out.println(shownWord);
+        
+        shownWord = checkChar("s", "Mississippi", shownWord);
+        System.out.println(shownWord);
     }
 }
